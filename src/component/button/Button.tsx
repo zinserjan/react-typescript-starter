928aad61b755c8d6ac5fd621672b34d654e9ccb3
Jan-André Zinser <zinserjan@gmail.com>
2017-06-05 22:53:37 +0200

    Use classnames

--- src/component/button/Button.tsx
+++ src/component/button/Button.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as styles from "./Button.module.scss";
+import * as classNames from "classnames";
 
 export interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {}
 
@@ -7,8 +8,13 @@ export interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {}
 const Button: React.StatelessComponent<ButtonProps> = (props: ButtonProps) => {
   const { ...others } = props;
 
+  const cssClass = classNames(styles.red, {
+    [styles.disabled]: false,
+    [styles.disabled2]: false,
+  });
+
   return (
-    <button {...others} className={styles.red}>
+    <button {...others} className={cssClass}>
       {props.children}
     </button>
   );
