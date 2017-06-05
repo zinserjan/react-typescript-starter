0571c54b97e009e416f8635c1b709022aa78d100
Jan-André Zinser <zinserjan@gmail.com>
2017-06-05 23:11:18 +0200

    Another component

---
+++ src/component/container/Container.story.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { storiesOf, module } from "@storybook/react";
+import Container from "./Container";
+
+storiesOf("Container", module)
+  .add("default", () => <Container />);
---
+++ src/component/container/Container.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Button from "component/button/Button";
+
+const Container: React.StatelessComponent<{}> = () => {
+  return (
+    <div>
+      <Button>Test</Button>
+    </div>
+  );
+};
+
+export default Container;
