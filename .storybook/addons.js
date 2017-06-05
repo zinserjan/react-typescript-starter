2a1fccb6796cee23b91107c1b64668e46cb017e8
Jan-André Zinser <zinserjan@gmail.com>
2017-06-06 01:08:26 +0200

    Add a11y check addon for storybook

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -2,3 +2,4 @@
 require("@storybook/addon-actions/register");
 require("@storybook/addon-links/register");
 require("@storybook/addon-knobs/register");
+require("storybook-addon-a11y/register");
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,11 @@
 const storybook = require("@storybook/react");
 const addonKnobs = require("@storybook/addon-knobs");
+const addonA11y = require("storybook-addon-a11y");
 
 // register knobs globally
 storybook.addDecorator(addonKnobs.withKnobs);
+// register a11y globally
+storybook.addDecorator(addonA11y.checkA11y);
 
 function requireAll(requireContext) {
   return requireContext.keys().map(requireContext);
