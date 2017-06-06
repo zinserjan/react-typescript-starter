009946448c9194ebdd850847d40650391d35c22f
Jan-André Zinser <zinserjan@gmail.com>
2017-06-06 23:32:26 +0200

    Use pre-commit hook to format the changed code with prettier

--- src/component/container/Container.story.tsx
+++ src/component/container/Container.story.tsx
@@ -2,5 +2,4 @@ import * as React from "react";
 import { storiesOf, module } from "@storybook/react";
 import Container from "./Container";
 
-storiesOf("Container", module)
-  .add("default", () => <Container />);
+storiesOf("Container", module).add("default", () => <Container />);
